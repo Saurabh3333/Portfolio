0c165a1500278fcf569dd1692d61dc773c86cf52
Saurabh Shubham <saurabh.friday@gmail.com>
2017-12-24 14:21:16 +0530

    front page complete

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/cv', (req, res) => {
+    res.send("Under construction");
+});
+
+app.get('/blog', (req, res) => {
+    res.send("Under construction");
+});
+
+app.get('/projects', (req, res) => {
+    res.send("Under construction");
+});
+
 
 
 exports.app = functions.https.onRequest(app);
